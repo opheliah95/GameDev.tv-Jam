a5ef64574473e893c6d775b22d5b878badaa6b33
Hyper Sonic <hs@fuwar.in>
2020-05-23 17:27:09 +0100

    [MENU] Binder dual page selection with fallback to None

--- Assets/Scripts/UI/CaseFile.cs
+++ Assets/Scripts/UI/CaseFile.cs
@@ -7,45 +7,37 @@ using CommonsHelper;
 public class CaseFile : MonoBehaviour
 {
     /* Child references */
+    
+    [Tooltip("Array of size 2 with Pages_Left and Pages_Right, in this order.")]
+    public CaseFileSide[] sides;
 
-    [Tooltip("Parent of all left page objects. Must contain pages in same order as the tabs, starting with None.")]
-    public Transform pagesLeftParent;
+    /// Array of all page transforms, whatever side they are on.
+    /// Start with None, then all "real" pages in same order as the tabs
+    private Transform[] pageTransforms;
     
     /* State */
 
     private bool m_Open = false;
     public bool IsOpen => m_Open;
-    
-    private CaseFilePage m_CurrentPageLeft = CaseFilePage.Brief;
-
-    private GameObject GetPage(CaseFilePage page)
-    {
-        var pageIndex = (int) page;
-        var pageCount = pagesLeftParent.childCount;
-        Debug.AssertFormat(pageIndex < pageCount,
-            "Page index {0} is not less than pages left child count {1}.",
-            pageIndex, pageCount);
-        
-        Transform pageTr = pagesLeftParent.GetChild(pageIndex);
-        return pageTr.gameObject;
-    }
-    
-    private void SetPageActive(CaseFilePage page, bool active)
-    {
-        GetPage(page).SetActive(active);
-    }
 
     private void Start()
     {
-        foreach (var caseFilePage in EnumUtil.GetValues<CaseFilePage>())
-        {
-            SetPageActive(caseFilePage, false);
-        }
+        // all pages should be on Pages_Left on start
+        Debug.AssertFormat(sides.Length == 2, this, "sides has length {0}, expected 2", sides.Length);
+        Debug.AssertFormat(sides[1].GetAllPages().Length == 0, sides[1], "Right Side has {0} child(ren), expected 0", sides[1].transform.childCount);
 
-        // should be Brief at this point
-        SetPageActive(m_CurrentPageLeft, true);
+        // store references to all pages on left side now, we'll move them later between sides
+        pageTransforms = sides[0].GetAllPages();
+        
+        // on left side, start by showing page 1 (Brief)
+        sides[0].HideAllPages();
+        sides[0].ShowPage(1, pageTransforms);
+        
+        // on right side, start by showing page 0 (None)
+        // there are no pages to start with, according to assertions above, so no need to hide first
+        sides[1].ShowPage(0, pageTransforms);
     }
-    
+
     public void Open()
     {
         m_Open = true;
@@ -61,21 +53,49 @@ public class CaseFile : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    public void SwitchToPage(CaseFilePage page)
+    /// Switch to target page on target side, moving it from another side if needed.
+    public void SwitchSideToPage(CaseFileSide side, int pageIndex)
     {
-        Debug.AssertFormat(m_Open, "CaseFile.OpenPage called on page {0} but CaseFile itself is not open yet.", page);
-
-        if (page != m_CurrentPageLeft)
+        // check that target page is not None
+        if (pageIndex == 0)
         {
-            SetPageActive(m_CurrentPageLeft, false);
-            m_CurrentPageLeft = page;
-            SetPageActive(page, true);
+            Debug.LogWarningFormat(this,
+                "Cannot show page index 0 (None) on side {0}, as there is only one None page " +
+                "and it couldn't be used as fallback page for the side already displaying it.", side);
+            return;
         }
-    }
-    
-    public void SwitchToPageByIndex(int pageIndex)
-    {
-        var page = (CaseFilePage) pageIndex;
-        SwitchToPage(page);
+        
+        // check that target page is not already shown on target side
+        if (pageIndex == side.CurrentPageIndex)
+        {
+            Debug.LogWarningFormat(this, "Side {0} already shows page {1}", side, pageIndex);
+            return;
+        }
+
+        // check if page is not already shown on the target side
+        Transform page = pageTransforms[pageIndex];
+        CaseFileSide oldSide = null;
+        if (page.gameObject.activeSelf && page.parent != side.pagesParent)
+        {
+            // Page is on another side at the moment, so we will move it to the target side
+            // in side.ShowOnlyPage more below.
+            // On the old side, we will fallback to page 0 (None).
+            // However, we wait for side.ShowOnlyPage below first so it doesn't re-hide
+            // the None page afterward.
+            oldSide = page.parent.parent.GetComponentOrFail<CaseFileSide>();
+        }
+
+        // show new page on target side, moving it if needed
+        side.ShowOnlyPage(pageIndex, pageTransforms);
+        
+        // check if page has moved
+        if (oldSide != null)
+        {
+            // We can now fallback to page 0 (None) on the old side.
+            // We must not hide what the old side thinks is the current page
+            // (it's now the target page on the new side), so we must use ShowPage
+            // instead of ShowOnlyPage.
+            oldSide.ShowPage(0, pageTransforms);
+        }
     }
 }
---
+++ Assets/Scripts/UI/CaseFileSide.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CommonsHelper;
+
+public class CaseFileSide : MonoBehaviour
+{
+    /* Parent references */
+    
+    [Tooltip("Parent case file")]
+    public CaseFile caseFile;
+    
+    /* Child references */
+
+    [Tooltip("Parent of all page objects on this side.")]
+    public Transform pagesParent;
+    
+    /// Current page on this side
+    private int m_CurrentPageIndex = 0;
+    public int CurrentPageIndex => m_CurrentPageIndex;
+
+    public Transform[] GetAllPages()
+    {
+        int childCount = pagesParent.childCount;
+        
+        var pageTransforms = new Transform[childCount];
+        
+        for (int i = 0; i < childCount; i++)
+        {
+            pageTransforms[i] = pagesParent.GetChild(i);
+        }
+
+        return pageTransforms;
+    }
+
+    public void HideAllPages()
+    {
+        foreach (Transform child in pagesParent)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+    
+    private void HideCurrentPage(Transform[] pageTransforms)
+    {
+        Debug.AssertFormat(m_CurrentPageIndex < pageTransforms.Length, this,
+            "We registered {0} pages, side {1} cannot hide current page {2}",
+            this, pageTransforms.Length, m_CurrentPageIndex);
+
+        pageTransforms[m_CurrentPageIndex].gameObject.SetActive(false);
+    }
+    
+    /// Show page of given index and transform on this side,
+    /// without hiding anything else (should only be called after HideAllPages)
+    public void ShowPage(int pageIndex, Transform[] pageTransforms)
+    {
+        Transform pageTransform = pageTransforms[pageIndex];
+        
+        // update model
+        m_CurrentPageIndex = pageIndex;
+        // move page to this side, preserving local position
+        pageTransform.SetParent(pagesParent, false);
+        // show page if not already active
+        pageTransform.gameObject.SetActive(true);
+    }
+    
+    /// Show page of given index and transform on this side,
+    /// hiding any current page
+    public void ShowOnlyPage(int pageIndex, Transform[] pageTransforms)
+    {
+        HideCurrentPage(pageTransforms);
+     
+        Debug.AssertFormat(pageIndex < pageTransforms.Length, this,
+            "We registered {0} pages, side {1} cannot show page {2}",
+            this, pageTransforms.Length, pageIndex);
+        
+        Transform pageTransform = pageTransforms[pageIndex];
+        
+        // update model
+        m_CurrentPageIndex = pageIndex;
+        // if needed, move page to this side, preserving local position
+        if (pageTransform.parent != pagesParent)
+        {
+            pageTransform.SetParent(pagesParent, false);
+        }
+        // show page if not already active
+        pageTransform.gameObject.SetActive(true);
+    }
+    
+    /// Switch to page by index, moving page from another side if needed.
+    /// Main entry function for case file page navigation, used by tab buttons.
+    public void SwitchToPage(int pageIndex)
+    {
+        caseFile.SwitchSideToPage(this, pageIndex);
+    }
+}
