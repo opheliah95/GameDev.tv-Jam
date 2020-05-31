e0fe04d7f49d15063264e76b9d6160941ed98fe7
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2020-05-31 03:36:06 +0100

    [MAINMENU] Updated menu background animation and assets related to menu

---
+++ Assets/Scripts/Menu/MenuCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour
+{
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * speed;
+    }
+}
