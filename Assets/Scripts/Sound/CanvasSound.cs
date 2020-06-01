2d5be045fa0e9a1c07a449b19171c8bda4940831
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2020-06-01 22:06:04 +0100

    [AUDIO] A clue sound will play everytime you examined a clue {SCRIPT] Added a play sound on awake script to dialogue box prefab

---
+++ Assets/Scripts/Sound/CanvasSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CanvasSound : MonoBehaviour
+{
+	AudioSource audioSource;
+	bool initialized;
+
+	void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+
+	}
+
+	void OnEnable()
+	{
+		audioSource.Play();
+	}
+}
