db744f964ac058c1b1cbe0dea14e4019cfcdd72d
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2020-06-06 23:52:18 +0100

    [AUDIO] Added function to play a random unlock sound when a scene/clue is unlocked

--- Assets/Scripts/Event/UnlockSceneEvent.cs
+++ Assets/Scripts/Event/UnlockSceneEvent.cs
@@ -1,6 +1,7 @@
 using UnityConstants;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class UnlockSceneEvent : GameplayEvent
 {
     [Tooltip("Scene to unlock")]
@@ -9,9 +10,33 @@ public class UnlockSceneEvent : GameplayEvent
     [Tooltip("Type of scene to unlock")]
     public CaseSceneType sceneType;
 
+    // a list of sound to play when unlock objects
+    public SoundData unlockSound;
+
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     protected override void Execute()
     {
+        playSound();
         CaseManager.Instance.CurrentCaseProgress.UnlockScene(sceneType, sceneEnum);
         End();
+    }
+
+    protected void playSound()
+    {
+        //stop any audio that is currently playing
+        audioSource.Stop();
+
+        //randomly generate a sound to play
+        int index = Random.Range(0, unlockSound.sounds.Count - 1);
+        audioSource.clip = unlockSound.sounds[index];
+
+        // play the sound
+        audioSource.Play();
+        
     }
 }
