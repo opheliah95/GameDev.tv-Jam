a5eb5aceb78269944135e3bd18653445b3829bb1
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2020-06-07 00:03:14 +0100

    [AUDIO] Created sound data with footstep clips and clue clips

--- Assets/Scripts/Sound/SoundData/SoundData.cs
+++ Assets/Scripts/Sound/SoundData/SoundData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SoundData", menuName ="ScriptableObject/Sound Data")]
+[CreateAssetMenu(fileName = "SoundData", menuName ="ScriptableObjects/Sound Data")]
 public class SoundData : ScriptableObject
 {
     public List<AudioClip> sounds;
