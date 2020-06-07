c52cf7ae9840e636d3b925779ae5a28f5edc682a
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2020-06-08 00:22:14 +0100

    [AUDIO] Updated find clue event, so that it can play a randomized sound

--- Assets/Scripts/Event/FindClueEvent.cs
+++ Assets/Scripts/Event/FindClueEvent.cs
@@ -6,8 +6,12 @@ public class FindClueEvent : GameplayEvent
     [Tooltip("Clue string ID")]
     public string clueStringID;
 
+    [Tooltip("Sound Data associated with the events")]
+    public SoundData soundsToPlay;
+
     protected override void Execute()
     {
+        SoundManager.Instance.startPlaySound(soundsToPlay); // play sound associated with clues
         CaseManager.Instance.CurrentCaseProgress.FindClue(clueStringID);
         End();
     }
